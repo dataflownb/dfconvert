b554db10f7b047242912f00bad7ce4000e260640
Colin Brown <cbrown12@umassd.edu>
2017-06-29 17:10:53 -0400

    major changes to package

--- dfipy_convert/export.py
+++ dfipy_convert/export.py
@@ -1,77 +1,81 @@
 import json
 import redbaron
-import sys
-from topological import topological
+import os
+from dfipy_convert.topological import topological
 
-#filename = str(sys.argv[1])
-fullpath = os.getcwd()
-with open(filename) as json_data:
-    d = json.load(json_data)
+def export_dfpynb(filename,d):
+    fullpath = os.getcwd()
+    edgelist = {}
+    cellindices = {}
+    cellflag = 0
+    prevind = ""
 
-edgelist = {}
-cellindices = {}
-cellflag = 0
-prevind = ""
-
-for count, cell in enumerate(d['cells']):
-    if (cell['cell_type'] != "code"):
+    for count, cell in enumerate(d['cells']):
+        if (cell['cell_type'] != "code"):
+            if (cellflag == 1):
+                cellindices[("umarkdown", prevind)] = count - 1
+                prevind = "umarkdown" + prevind
+            cellflag = 1
+            continue
+        nlist = ""
+        mlist = []
+        exec_count = hex(cell['execution_count'])[2:]
+        for line in cell['source'].split('\n'):
+            if (line[0] == '%'):
+                mlist.append(line + '\n')
+                continue
+            nlist = nlist + line.rstrip() + '\n'
+        nlist = redbaron.RedBaron(nlist)
         if (cellflag == 1):
-            cellindices[("umarkdown", prevind)] = count - 1
-            prevind = "umarkdown" + prevind
-        cellflag = 1
-        continue
-    nlist = ""
-    mlist = []
-    exec_count = hex(cell['execution_count'])[2:]
-    for line in cell['source']:
-        if (line[0] == '%'):
-            mlist.append(line + '\n')
-            continue
-        nlist = nlist + line.rstrip() + '\n'
-    nlist = redbaron.RedBaron(nlist)
-    if (cellflag == 1):
-        cellindices[("markdown", exec_count)] = count - 1
-        cellflag = 0
-    cellindices[exec_count] = count
-    edgelist[exec_count] = []
-    for node in nlist.find_all("name", value="Out"):
-        edgelist[exec_count].append(node.parent.value[1].value.value.strip("'").strip('"'))
-        newnodeval = "Out_" + node.parent.value[1].value.value.strip("'").strip('"')
-        node.value = newnodeval
-        del node.parent.value[1]
-    if (len(cell['outputs']) > 0 and len(nlist) > 0):
-        listlen = len(nlist) + len(mlist) - 1
-        nlist[listlen].insert_after("Out_" + exec_count + " = " + nlist[listlen].dumps())
-        nlist[listlen + 1].insert_after("Out_" + exec_count)
-        # print("Out_" + exec_count + " = " + nlist[len(nlist)-2].dumps())
-    for x in range(len(nlist)):
-        mlist.append((nlist[x].dumps()) + '\n')
-    mlist.append("###Out_" + exec_count + "###\n")
-    prevind = exec_count
-    cell['source'] = mlist
+            cellindices[("markdown", exec_count)] = count - 1
+            cellflag = 0
+        cellindices[exec_count] = count
+        edgelist[exec_count] = []
+        for node in nlist.find_all("name", value="Out"):
+            edgelist[exec_count].append(node.parent.value[1].value.value.strip("'").strip('"'))
+            newnodeval = "Out_" + node.parent.value[1].value.value.strip("'").strip('"')
+            node.value = newnodeval
+            del node.parent.value[1]
+        if (len(cell['outputs']) > 0 and len(nlist) > 0):
+            listlen = len(nlist) + len(mlist) - 1
+            nlist[listlen].insert_after("Out_" + exec_count + " = " + nlist[listlen].dumps())
+            nlist[listlen + 1].insert_after("Out_" + exec_count)
+            # print("Out_" + exec_count + " = " + nlist[len(nlist)-2].dumps())
+        for x in range(len(nlist)):
+            mlist.append((nlist[x].dumps()) + '\n')
+        mlist.append("###Out_" + exec_count + "###\n")
+        prevind = exec_count
+        cell['source'] = mlist
 
-topological = list(reversed(topological(edgelist)))
+    topologic = list(reversed(topological(edgelist)))
 
-for index in cellindices:
-    if (index[0] == 'markdown'):
-        topological.insert(topological.index(index[1]), index)
-    elif (index[0] == 'umarkdown'):
-        umcount = str.count(index[1], 'umarkdown')
-        # Special case single markdown cell at top of page
-        if (index == 'umarkdown'):
-            topological.insert(0, index)
-            continue
-        topological.insert((topological.index(index[1][umcount * len(index[0]):])), index)
+    for index in cellindices:
+        if (index[0] == 'markdown'):
+            topologic.insert(topologic.index(index[1]), index)
+        elif (index[0] == 'umarkdown'):
+            umcount = str.count(index[1], 'umarkdown')
+            # Special case single markdown cell at top of page
+            if (index == 'umarkdown'):
+                topologic.insert(0, index)
+                continue
+            topologic.insert((topologic.index(index[1][umcount * len(index[0]):])), index)
 
-origlist = d['cells'].copy()
-for count, cellno in enumerate(topological):
-    cell = origlist[cellindices[cellno]]
-    if (cell['cell_type'] == "code"):
-        cell['execution_count'] = count + 1
-        if (len(cell['outputs']) > 0):
-            cell['outputs'][0]['execution_count'] = count + 1
-    d['cells'][count] = cell
+    origlist = d['cells'].copy()
+    for count, cellno in enumerate(topologic):
+        cell = origlist[cellindices[cellno]]
+        if (cell['cell_type'] == "code"):
+            cell['execution_count'] = count + 1
+            if (len(cell['outputs']) > 0):
+                cell['outputs'][0]['execution_count'] = count + 1
+        d['cells'][count] = cell
 
-filename = fullpath + '\' + filename[:-6] + "_ipy" + filename[-6:]
-with open(filename, 'w') as outfile:
-    json.dump(d, outfile, indent=4)
+    filename = os.path.join(fullpath, filename[:-6] + "_ipy" + filename[-6:])
+    with open(filename, 'w') as outfile:
+        json.dump(d, outfile, indent=4)
+    return filename
+
+def bundle(handler, model):
+    """Converts the existing IPython Notebook file into a Dataflow Kernel File"""
+    notebook_filename = model['name']
+    notebook_content = model['content']
+    handler.finish('File Exported As: {}'.format(export_dfpynb(notebook_filename,notebook_content)))
--- dfipy_convert/import.py
+++ dfipy_convert/import.py
@@ -1,48 +1,54 @@
 import json
 import redbaron
-import sys
 import re
 import os, binascii
 
-#filename = str(sys.argv[1])
-fullpath = os.getcwd()
-with open(filename) as json_data:
-    d = json.load(json_data)
-
-for cell in d['cells']:
-    if(cell['cell_type'] != "code"):
-        continue
-    nlist = ""
-    mlist = []
-    exec_count = int(binascii.b2a_hex(os.urandom(6)),16)
-    for line in cell['source']:
-        if(line[0] == '%'):
-            mlist.append(line+'\n')
+def import_dfpynb(filename,d):
+    fullpath = os.getcwd()
+    for cell in d['cells']:
+        if (cell['cell_type'] != "code"):
             continue
-        elif(re.search('###Out_[0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f]###',line) != None):
-            exec_count = int(re.search('(?<=###Out_)[0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f](?<!###)',line).group(0),16)
-            continue
-        nlist =  nlist + line.rstrip() + '\n'
-    nlist = redbaron.RedBaron(nlist)
-    for node in nlist.find_all("name",value=lambda value: re.search('(?<=Out_)'+hex(exec_count)[2:],value)):
-        try:
-            val = nlist.index(node.parent)
-        except ValueError:
-            pass
-        del nlist[val]
-    for node in nlist.find_all("name",value=lambda value: re.search('(?<=Out_)[0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f]',value)):
-        node.value = "Out['"+str(node.value)[len("Out_"):]+"']"
-    cell['execution_count'] = exec_count
-    if(len(cell['outputs']) > 0):
-        cell['outputs'][0]['execution_count'] = exec_count
-    for x in range(len(nlist)):
-        mlist.append((nlist[x].dumps()) + '\n')
-    cell['source'] = mlist
+        nlist = ""
+        mlist = []
+        exec_count = int(binascii.b2a_hex(os.urandom(6)), 16)
+        for line in cell['source'].split('\n'):
+            if (line[0] == '%'):
+                mlist.append(line + '\n')
+                continue
+            elif (re.search('###Out_[0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f]###',
+                            line) != None):
+                exec_count = int(
+                    re.search('(?<=###Out_)[0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f](?<!###)',
+                              line).group(0), 16)
+                continue
+            nlist = nlist + line.rstrip() + '\n'
+        nlist = redbaron.RedBaron(nlist)
+        for node in nlist.find_all("name", value=lambda value: re.search('(?<=Out_)' + hex(exec_count)[2:], value)):
+            try:
+                val = nlist.index(node.parent)
+            except ValueError:
+                pass
+            del nlist[val]
+        for node in nlist.find_all("name", value=lambda value: re.search(
+                '(?<=Out_)[0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f]', value)):
+            node.value = "Out['" + str(node.value)[len("Out_"):] + "']"
+        cell['execution_count'] = exec_count
+        if (len(cell['outputs']) > 0):
+            cell['outputs'][0]['execution_count'] = exec_count
+        for x in range(len(nlist)):
+            mlist.append((nlist[x].dumps()) + '\n')
+        cell['source'] = mlist
 
+    if (re.search('_ipy', filename) != None):
+        ipy = str.index(filename, '_ipy')
+        filename = filename[:ipy] + filename[ipy + len('_ipy'):]
+    filename = os.path.join(fullpath, filename[:-6] + "_dfpy" + filename[-6:])
+    with open(filename, 'w') as outfile:
+        json.dump(d, outfile, indent=4)
+    return filename
 
-if(re.search('_ipy',filename) != None):
-    ipy = str.index(filename,'_ipy')
-    filename = filename[:ipy] + filename[ipy+len('_ipy'):]
-filename = fullpath + '\' + filename[:-6] + "_dfpy" + filename[-6:]
-with open(filename, 'w') as outfile:
-    json.dump(d,outfile,indent=4)
+def bundle(handler, model):
+    """Converts the existing IPython Notebook file into a Dataflow Kernel File"""
+    notebook_filename = model['name']
+    notebook_content = model['content']
+    handler.finish('File Exported As: {}'.format(import_dfpynb(notebook_filename,notebook_content)))
